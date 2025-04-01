72c893c48a0562dbefc4b63da86ee10977d727f4
Anil Kumar K <anilkumarkrishna027@gmail.com>
2025-04-01 16:53:44 +0530

    up

--- core/resources/views/templates/basic/sections/cta.blade.php
+++ core/resources/views/templates/basic/sections/cta.blade.php
@@ -1,7 +1,7 @@
 @php
     $content = getContent('cta.content', true);
 @endphp
-<section class="pt-120 pb-120 position-relative bg_img overlay-one"
+{{-- <section class="pt-120 pb-120 position-relative bg_img overlay-one"
     data-background="{{ frontendImage('cta', @$content->data_values->image, '730x465') }}">
     <!--<div class="bottom-shape"><img src="{{ asset($activeTemplateTrue . 'images/top-shape.png') }}" alt="@lang('image')">-->
     <!--</div>-->
@@ -14,5 +14,14 @@
                     href="{{ @$content->data_values->button_url }}">{{ __(@$content->data_values->button_title) }}</a>
             </div>
         </div>
+    </div>
+</section> --}}
+
+<section class="pt-120 pb-120">
+    <div class="text-center d-flex justify-content-center align-items-center m-3 position-relative">
+        <video width="100%" height="100%" muted autoplay playsinline class="rounded" loop >
+            <source src="/assets/videos/volunteer_bannar_for_daankart.mp4"  type="video/mp4">
+        </video>
+        <a class="position-absolute top-0 bottom-0 start-0 end-0" href="/volunteer/join-as/volunteer " ></a>
     </div>
 </section>
