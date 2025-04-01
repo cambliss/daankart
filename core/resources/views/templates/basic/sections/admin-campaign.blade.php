6dab0a6293eca0bd43ef13c6c1c91530f5ad2b47
Anil Kumar K <anilkumarkrishna027@gmail.com>
2025-04-01 17:11:39 +0530

    up

--- core/resources/views/templates/basic/sections/admin-campaign.blade.php
+++ core/resources/views/templates/basic/sections/admin-campaign.blade.php
@@ -118,12 +118,6 @@
         </div>
     </section>
     <!--section end -->
-
-    @if ($sections->secs != null)
-        @foreach (json_decode($sections->secs) as $sec)
-            @include($activeTemplate . 'sections.' . $sec)
-        @endforeach
-    @endif
 @endsection
 
 @push('script-lib')
