0eadb4666ad30b12ea6d17256613b1d8ed3bbece
Anil Kumar K <anilkumarkrishna027@gmail.com>
2025-04-01 16:33:16 +0530

    up

--- core/resources/views/templates/basic/sections/page_banner.blade.php
+++ core/resources/views/templates/basic/sections/page_banner.blade.php
@@ -4,28 +4,17 @@ $mappedBanner = [
     'contact' => 'assets/images/frontend/contact_banner/contact_banner.jpg',
 ];
 ?>
-<div class="page-banner-section banner-container">
+<section class="pb-120 pt-5 page-banner-section">
     @if (isset($pageSlug) && isset($mappedBanner[$pageSlug]))
         <img src="{{ asset($mappedBanner[$pageSlug]) }}" alt="explore_campaign" class="banner-image">
     @endif
-</div>
+</section>
 @push('style')
     <style>
-        .page-banner-section.banner-container {
-            padding-bottom: 120px;
-            padding-top: 120px;
-            width: 100%;
-            height: 380px;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-        }
-
         .page-banner-section .banner-image {
             width: 100%;
             height: 100%;
-            object-fit: cover;
+            object-fit: contain;
         }
     </style>
 @endpush
